a0fb313ca8329bdb8dd4e381fabaeea9aaabece5
Thiago-Heleno <64995223+Thiago-Heleno@users.noreply.github.com>
2023-10-01 21:58:13 -0300

    Added Gui

--- main.py
+++ main.py
@@ -1,92 +1,141 @@
 import asyncio
+import os
 import random
+import subprocess
 import threading
 import time
 
+import customtkinter
 import requests
-from playwright.async_api import async_playwright
-from playwright_stealth import stealth_async
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-class MultiPost:
-    def __init__(self):
-        pass
+def main(socialMedia: str):
+    if socialMedia == "TikTok":
+        Tiktok()
+        print("Tiktok")
+    if socialMedia == "Instagram":
+        # Instagram()
+        print("IG")
+    if socialMedia == "YouTube":
+        # Youtube()
+        print("Youtube")
 
-    async def main(self, socialMedia: str):
-        print(socialMedia)
-        async with async_playwright() as p:
-            browser_type = p.chromium
-            # browser_type.LaunchOptions().setExecutablePath(
-            #     "C:\\Users\\theki\AppData\\Local\\Chromium\\Application\\chrome.exe"
-            # )
-            self.browser = await browser_type.launch(
-                headless=False,
-                executable_path="C:\\Users\\theki\AppData\\Local\\Chromium\\Application\\chrome.exe",
+    return
+
+
+def Tiktok():
+    try:
+        browser.get("https://www.tiktok.com/upload?lang=pt-BR")
+
+        # Wait for the iframe to be present before switching to it
+        iframe = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "iframe"))
+        )
+        browser.switch_to.frame(iframe)
+
+        # Locate the file input element and upload a file
+        file_input = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="root"]/div/div/div/div/div/div/div/input')
             )
-            self.context = await self.browser.new_context()
-            self.page = await self.context.new_page()
-            await stealth_async(self.page)
+        )
+        file_input.send_keys(os.getcwd() + "/data/video.mp4")
 
-            extra_headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-                "Accept-Language": "pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-            }
-            await self.page.set_extra_http_headers(extra_headers)
+        # Click the upload button
+        upload_button = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="root"]/div/div/div/div/div/div/div/input')
+            )
+        )
+        upload_button.click()
 
-            if socialMedia == "TikTok":
-                await self.Tiktok()
-            if socialMedia == "Instagram":
-                await self.Instagram()
-            if socialMedia == "YouTube":
-                await self.Youtube()
+        videoTitleInput = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//*[@id="root"]/div/div/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div[2]/div/div/div/div/div/div/div/div/div/span/span',
+                )
+            )
+        )
+        # Set the inner text of the element using JavaScript
+        new_text = "New Video Title"
+        browser.execute_script(
+            "arguments[0].innerText = arguments[1];", videoTitleInput, new_text
+        )
 
-            await asyncio.sleep(random.uniform(4, 5))
-            await self.browser.close()
+        uploadBtn = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//*[@id="root"]/div/div/div/div[2]/div[2]/div[2]/div[8]/div[2]',
+                )
+            )
+        )
+        uploadBtn.click()
 
-    async def Tiktok(self):
-        try:
-            await self.page.goto("https://www.tiktok.com/explore")
-        except:
-            print("Site didn't load")
-            await self.context.close()
-            return False
+        print("DONE")
 
-        await asyncio.sleep(random.uniform(4, 5))
-        await self.page.wait_for_load_state("networkidle")
-
-    async def Instagram(self):
-        try:
-            await self.page.goto("https://www.instagram.com")
-        except:
-            print("Site didn't load")
-            await self.context.close()
-            return False
-
-        await asyncio.sleep(random.uniform(4, 5))
-        await self.page.wait_for_load_state("networkidle")
-
-    async def Youtube(self):
-        try:
-            await self.page.goto("https://www.youtube.com")
-        except:
-            print("Site didn't load")
-            await self.context.close()
-            return False
-
-        await asyncio.sleep(random.uniform(25, 30))
-        await self.page.wait_for_load_state("networkidle")
-
-    def thread_function(self, socialMedia: str):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(self.main(socialMedia))
-
-    def start(self):
-        social_medias = ["TikTok", "Instagram", "YouTube"]
-        for social_media in social_medias:
-            threading.Thread(target=self.thread_function, args=(social_media,)).start()
-            time.sleep(1)
+        browser.switch_to.default_content()
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return False
+    finally:
+        browser.quit()
 
 
-if __name__ == "__main__":
-    MultiPost().start()
+def Instagram():
+    try:
+        browser.get("https://www.instagram.com")
+    except:
+        print("Site didn't load")
+        return False
+
+
+def Youtube():
+    try:
+        browser.get("https://www.youtube.com")
+    except:
+        print("Site didn't load")
+        return False
+
+
+# Init Variables and Browser
+service = Service(
+    executable_path="C:\\Users\\theki\\OneDrive\\Documents\\dyrkClothing\\MultiUpload\\chromedriver.exe"
+)
+
+chrome_options = Options()
+chrome_options.add_argument(
+    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+)
+chrome_options.add_experimental_option("debuggerAddress", "localhost:9222")
+
+browser = webdriver.Chrome(options=chrome_options, service=service)
+
+
+def Start():
+    social_medias = ["TikTok", "Instagram", "YouTube"]
+    for social_media in social_medias:
+        main(social_media)
+
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("dark-blue")
+root = customtkinter.CTk()
+root.title("MultiPost")
+root.geometry("500x350")
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+label = customtkinter.CTkLabel(master=frame, text="MultiPost", font=("Roboto", 24))
+label.pack(pady=12, padx=10)
+button = customtkinter.CTkButton(master=frame, text="Upload", command=Start)
+button.pack(pady=12, padx=10)
+
+root.mainloop()
