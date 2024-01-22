64f1df54328ebde8497591654803fc7c19248d14
Thiago-Heleno <64995223+Thiago-Heleno@users.noreply.github.com>
2024-01-22 12:30:07 -0300

    v1.0.0

--- localhost/Default/Extensions/ihcjicgdanjaechkgeegckofjjedodee/2.6.17_0/utils/sentry/sentryInit.js
+++
@@ -1,1 +0,0 @@
-import{chrome}from"../polyfill";export function sentryInit(e={}){let t={dsn:"https://708a324f1a7ec87a2658346fb4bb7586@o438337.ingest.sentry.io/4506062147551232",tracesSampleRate:.6,replaysSessionSampleRate:0,replaysOnErrorSampleRate:0,environment:ENVIRONMENT,enabled:!1,release:chrome.runtime.getManifest().version};Object.assign(t,e)}
